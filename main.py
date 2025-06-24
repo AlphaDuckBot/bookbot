ac967aa27d2573aff89a27067572471d1922f64b
AlphaDuckBot <lkverlaan@gmail.com>
2025-06-24 11:22:23 +0200

    C: add character counter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from stats import get_num_of_words
-
+from stats import get_num_of_words, count_characters
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -10,6 +9,8 @@ def main():
     txt = get_book_text("books/frankenstein.txt")
     num_of_words = get_num_of_words(txt)
     print(f"{num_of_words} words found in the document")
+    count_char_dict = count_characters(txt)
+    print(count_char_dict)
     #print(txt)
 
 main( )
--- stats.py
+++ stats.py
@@ -2,3 +2,15 @@ def get_num_of_words(text_string):
     list = text_string.split()
     amount = len(list)
     return amount
+
+def count_characters(text_string):
+    output_dictionary = dict()
+    lowercase_string = text_string.lower()
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    for i in range(0, len(alphabet)):
+        counter = 0
+        for char in lowercase_string:
+            if char in alphabet[i]:
+                counter += 1
+        output_dictionary[f"{alphabet[i]}"] = counter
+    return output_dictionary
