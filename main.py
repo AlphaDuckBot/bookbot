88b9cc10e84accf63becfeeb42e634db398c09d6
AlphaDuckBot <lkverlaan@gmail.com>
2025-06-24 10:42:59 +0200

    B: add word counter

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from stats import get_num_of_words
+
+
 def get_book_text(filepath):
     with open(filepath) as f:
         output = f.read()
@@ -5,7 +8,9 @@ def get_book_text(filepath):
 
 def main():
     txt = get_book_text("books/frankenstein.txt")
-    print(txt)
+    num_of_words = get_num_of_words(txt)
+    print(f"{num_of_words} words found in the document")
+    #print(txt)
 
 main( )
 
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_num_of_words(text_string):
+    list = text_string.split()
+    amount = len(list)
+    return amount
