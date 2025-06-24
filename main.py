9796f4f942564f59ff3ea90dba096bdd167e117f
AlphaDuckBot <lkverlaan@gmail.com>
2025-06-24 14:35:17 +0200

    D: Finished bookbot

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 from stats import get_num_of_words, count_characters
 
-def get_book_text(filepath):
-    with open(filepath) as f:
+import sys
+
+def get_book_text(file_to_path):
+    with open(file_to_path) as f:
         output = f.read()
         return output
 
+def sort_on(items):
+    return items["num"]
+
 def main():
-    txt = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
+
+    txt = get_book_text(filepath)
     num_of_words = get_num_of_words(txt)
-    print(f"{num_of_words} words found in the document")
-    count_char_dict = count_characters(txt)
-    print(count_char_dict)
+
+    count_char_list = count_characters(txt)
+    count_char_list.sort(reverse=True, key=sort_on)
+
     #print(txt)
 
-main( )
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_of_words} total words")
+    print("--------- Character Count -------")
+    for i in range(0, len(count_char_list)):
+        tmp = count_char_list[i]
+        print(f"{tmp["char"]}: {tmp["num"]}")
+    print("============= END ===============")
+
+main()
+
 
 
--- stats.py
+++ stats.py
@@ -5,12 +5,19 @@ def get_num_of_words(text_string):
 
 def count_characters(text_string):
     output_dictionary = dict()
+    the_list = []
     lowercase_string = text_string.lower()
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    
     for i in range(0, len(alphabet)):
         counter = 0
         for char in lowercase_string:
             if char in alphabet[i]:
                 counter += 1
-        output_dictionary[f"{alphabet[i]}"] = counter
-    return output_dictionary
+
+        output_dictionary["char"] = f"{alphabet[i]}"
+        output_dictionary["num"] = counter
+        the_list.append(output_dictionary)
+        output_dictionary = dict()
+        
+    return the_list
